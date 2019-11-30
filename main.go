9b101da30a79865b697cddb2957079d915248042
GeoIrb <morochev.g@gmail.com>
2019-12-01 00:43:43 +0300

    creating server and client

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
-/*
-URL: https://github.com/mccoyst/myip/blob/master/myip.go
-URL: http://changsijay.com/2013/07/28/golang-get-ip-address/
-*/
-
 import (
-	"net"
-	"os"
+	"fmt"
+
+	"github.com/GeoIrb/mqtt-cert/generate"
 )
 
 func main() {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
-		os.Exit(1)
-	}
-
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To16() != nil {
-				os.Stdout.WriteString(ipnet.IP.String() + "\n")
-			}
-		}
-	}
+	ca, err := generate.NewCertificateAuthority()
+	fmt.Println(err, ca.Server(), ca.Client())
 }
